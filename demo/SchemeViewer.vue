276993eb5a943f2f030ae7f0e6b806a255dc3de7
Baku Hashimoto <mail@baku89.com>
2022-06-09 23:23:28 +0900

    Update SchemeViewer

--- demo/SchemeViewer.vue
+++ demo/SchemeViewer.vue
@@ -1,108 +1,76 @@
 <template lang='pug'>
-dl.SchemeViewer
-	div
-		dt.border(style='background: rgb(var(--base00))') 00
-		dd BG
-	div
-		dt(style='background: rgb(var(--base01))') 01
-		dd Input BG
-	div
-		dt(style='background: rgb(var(--base02))') 02
-		dd Selection
-	div
-		dt(style='background: rgb(var(--base03))') 03
-		dd Comments, Line Highlighting
-	div
-		dt.invert(style='background: rgb(var(--base04))') 04
-		dd Text, Label
-	div
-		dt.invert(style='background: rgb(var(--base05))') 05
-		dd FG, Caret, Delimiters, Operators
-	div
-		dt.invert(style='background: rgb(var(--base06))') 06
-		dd Input Value
-	div
-		dt.invert(
-			:class='{accent: baseAccentName === "07"}',
-			style='background: rgb(var(--base07))'
-		).
+ul.SchemeViewer
+	li
+		.color.border(style='background: rgb(var(--base00))') 00
+		.caption BG
+	li
+		.color(style='background: rgb(var(--base01))') 01
+		.caption Input BG
+	li
+		.color(style='background: rgb(var(--base02))') 02
+		.caption Selection
+	li
+		.color(style='background: rgb(var(--base03))') 03
+		.caption Comments, Invisibles, Line Highlighting
+	li
+		.color.invert(style='background: rgb(var(--base04))') 04
+		.caption Dark FG, Status Bar
+	li
+		.color.invert(style='background: rgb(var(--base05))') 05
+		.caption FG, Caret, Delimiters, Operators
+	li
+		.color.invert(style='background: rgb(var(--base06))') 06
+		.caption Light FG
+	li
+		.color.invert(style='background: rgb(var(--base07))').
 			07
-		dd Light BG
-	div
-		dt.invert(
-			style='background: rgb(var(--base08))',
-			:class='{accent: baseAccentName === "08"}'
-		).
+		.caption Light BG
+	li
+		.color.invert(style='background: rgb(var(--base08))').
 			08
 
-		dd Variables, Deleted
-	div
-		dt.invert(
-			style='background: rgb(var(--base09))',
-			:class='{accent: baseAccentName === "09"}'
-		).
+		.caption Variables, Deleted
+	li
+		.color.invert(style='background: rgb(var(--base09))').
 			09
 
-		dd Integers, Boolean, Constants
-	div
-		dt.invert(
-			style='background: rgb(var(--base0A))',
-			:class='{accent: baseAccentName === "0A"}'
-		).
+		.caption Integers, Boolean, Constants
+	li
+		.color.invert(style='background: rgb(var(--base0A))').
 			0A
 
-		dd Classes, Markup Bold, Search Text Background
-	div
-		dt.invert(
-			style='background: rgb(var(--base0B))',
-			:class='{accent: baseAccentName === "0B"}'
-		).
+		.caption Classes, Markup Bold, Search Text Background
+	li
+		.color.invert(style='background: rgb(var(--base0B))').
 			0B
 
-		dd Strings, Inserted
-	div
-		dt.invert(
-			style='background: rgb(var(--base0C))',
-			:class='{accent: baseAccentName === "0C"}'
-		).
+		.caption Strings, Inserted
+	li
+		.color.invert(style='background: rgb(var(--base0C))').
 			0C
 
-		dd Support, Regular Expressions, Escape Characters
-	div
-		dt.invert(
-			style='background: rgb(var(--base0D))',
-			:class='{accent: baseAccentName === "0D"}'
-		).
+		.caption Support, Regex, Escape Characters
+	li
+		.color.invert(style='background: rgb(var(--base0D))').
 			0D
 
-		dd Functions, Headings
-	div
-		dt.invert(
-			style='background: rgb(var(--base0E))',
-			:class='{accent: baseAccentName === "0E"}'
-		).
+		.caption Functions, Headings
+	li
+		.color.invert(style='background: rgb(var(--base0E))').
 			0E
 
-		dd Keywords, Changed
-	div
-		dt.invert(
-			style='background: rgb(var(--base0F))',
-			:class='{accent: baseAccentName === "0F"}'
-		).
+		.caption Keywords, Changed
+	li
+		.color.invert(style='background: rgb(var(--base0F))').
 			0F
-		dd Deprecated
+		.caption Deprecated
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-	props: {
-		baseAccentName: {
-			type: String,
-			required: true,
-		},
-	},
+	name: 'SchemeViewer',
 })
 </script>
 
@@ -111,17 +79,16 @@ export default defineComponent({
 
 .SchemeViewer
 	position relative
-	display grid
-	grid-template-columns 100%
-	grid-template-rows repeat(16, 3em)
-	grid-auto-flow column
-	gap 0.3em
+	column-width 25em
+	column-fill balance-all
 
-	div
+	li
 		display grid
+		margin-bottom 0.3em
+		break-inside avoid
 		grid-template-columns 3em 1fr
 
-	dt
+	.color
 		border-radius var(--input-border-radius)
 		text-align center
 		font-size 1.4em
@@ -137,6 +104,6 @@ export default defineComponent({
 		&.accent
 			box-shadow inset 0 0 0 1px base16('04'), 0 0 0 1px base16('04'), inset 0 0 0 2px base16('00')
 
-	dd
-		padding-left 1em
+	.caption
+		padding-left 0.5em
 </style>
