c926c695c25af4623d664e28740469cf78c81175
Baku Hashimoto <mail@baku89.com>
2023-10-02 17:16:47 +0900

    Update Tweeq

---
+++ src/InputCubicBezier/index.ts
@@ -0,0 +1,4 @@
+import InputCubicBezier from './InputCubicBezier.vue'
+import InputCubicBezierPicker from './InputCubicBezierPicker.vue'
+
+export {InputCubicBezier, InputCubicBezierPicker}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-	console.log('** mode **', mode)
 	return {
 		plugins: [vue()],
 		resolve: {
