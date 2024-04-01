a463bf72a97006fc636a988aa9256000b612898b
Baku Hashimoto <mail@baku89.com>
2024-04-01 23:46:58 +0900

    Add useCursorStyle

---
+++ src/use/useCursorStyle.ts
@@ -0,0 +1,39 @@
+import {uniqueId} from 'lodash'
+import {
+	computed,
+	MaybeRefOrGetter,
+	onBeforeUnmount,
+	ref,
+	unref,
+	watchEffect,
+} from 'vue'
+
+const styles = ref<Map<string, string | null>>(new Map())
+
+const currentStyle = computed(() => {
+	for (const style of styles.value.values()) {
+		if (style !== null) return style
+	}
+	return 'inherit'
+})
+
+watchEffect(() => {
+	document.documentElement.style.cursor = currentStyle.value
+})
+
+export function useCursorStyle(
+	cursor: MaybeRefOrGetter<string | null> | (() => string | null)
+) {
+	const cursorRef: MaybeRefOrGetter =
+		typeof cursor === 'function' ? computed(cursor) : cursor
+
+	const id = uniqueId()
+
+	watchEffect(() => {
+		styles.value.set(id, unref(cursorRef))
+	})
+
+	onBeforeUnmount(() => {
+		styles.value.delete(id)
+	})
+}
