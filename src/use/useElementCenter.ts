1d1457584bc5e0def4e72f72f44d0024b3d42372
Baku Hashimoto <mail@baku89.com>
2024-04-01 23:47:04 +0900

    Add useElementCenter

---
+++ src/use/useElementCenter.ts
@@ -0,0 +1,14 @@
+import {MaybeComputedElementRef, useElementBounding} from '@vueuse/core'
+import {scalar, vec2} from 'linearly'
+import {computed} from 'vue'
+
+export function useElementCenter(el: MaybeComputedElementRef) {
+	const {left, top, right, bottom} = useElementBounding(el)
+
+	return computed<vec2>(() => {
+		return [
+			scalar.lerp(left.value, right.value, 0.5),
+			scalar.lerp(top.value, bottom.value, 0.5),
+		]
+	})
+}
