4b3f2333e2cfc6bfd3a65ba2e4eb2a0725b6f773
Baku Hashimoto <mail@baku89.com>
2024-07-11 07:13:11 +0900

    Update PaneZUI

--- src/PaneZUI.vue
+++ src/PaneZUI.vue
@@ -2,9 +2,14 @@
 import {Rect} from '@baku89/pave'
 import {useElementBounding} from '@vueuse/core'
 import {mat2d, vec2} from 'linearly'
-import {computed, ref, shallowRef, watchEffect} from 'vue'
+import {computed, ref, shallowRef, watch, watchEffect} from 'vue'
 
 import {useZUI} from './use/useZUI'
+
+const props = defineProps<{
+	transform?: mat2d
+	background?: 'dots'
+}>()
 
 const emit = defineEmits<{
 	'update:transform': [mat2d]
@@ -15,14 +20,21 @@ const $root = ref<HTMLElement | null>(null)
 
 const {width: rootWidth, height: rootHeight} = useElementBounding($root)
 
-const transform = shallowRef(mat2d.I)
+const transformLocal = shallowRef(mat2d.I)
+
+watch(
+	() => props.transform,
+	transform => {
+		transformLocal.value = transform ?? mat2d.I
+	}
+)
 
 watchEffect(() => {
-	emit('update:transform', transform.value)
+	emit('update:transform', transformLocal.value)
 })
 
 watchEffect(() => {
-	const invTransform = mat2d.inv(transform.value) ?? mat2d.I
+	const invTransform = mat2d.inv(transformLocal.value) ?? mat2d.I
 
 	const topLeft = vec2.transformMat2d([0, 0], invTransform)
 	const bottomRight = vec2.transformMat2d(
@@ -34,11 +46,11 @@ watchEffect(() => {
 })
 
 useZUI($root, delta => {
-	transform.value = mat2d.mul(delta, transform.value)
+	transformLocal.value = mat2d.mul(delta, transformLocal.value)
 })
 
 const rootStyles = computed(() => {
-	const [a, , , d, tx, ty] = transform.value
+	const [a, , , d, tx, ty] = transformLocal.value
 
 	const size = 20
 
@@ -50,13 +62,18 @@ const rootStyles = computed(() => {
 
 const transformStyles = computed(() => {
 	return {
-		transform: `matrix(${transform.value.join(',')})`,
+		transform: `matrix(${transformLocal.value.join(',')})`,
 	}
 })
 </script>
 
 <template>
-	<div ref="$root" class="PaneZUI" :style="rootStyles">
+	<div
+		ref="$root"
+		class="PaneZUI"
+		:class="{dots: background === 'dots'}"
+		:style="rootStyles"
+	>
 		<div class="transform" :style="transformStyles">
 			<slot />
 		</div>
@@ -70,9 +87,11 @@ const transformStyles = computed(() => {
 	overflow hidden
 	width 100%
 	height 100%
-	--bg var(--tq-color-background)
-	background-image linear-gradient(to right, transparent 1px, var(--bg) 1px), linear-gradient(to bottom, transparent 1px, var(--bg) 1px)
-	background-color var(--tq-color-gray-on-background)
+
+	&.dots
+		--bg var(--tq-color-background)
+		background-image linear-gradient(to right, transparent 1px, var(--bg) 1px), linear-gradient(to bottom, transparent 1px, var(--bg) 1px)
+		background-color var(--tq-color-gray-on-background)
 
 .transform
 	position absolute
