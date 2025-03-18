3d244e6ed4a77a4b9e8847aadbe746fe698720d9
Baku Hashimoto <mail@baku89.com>
2025-03-19 00:59:06 +0900

    Add full-screen feature to InputExample

--- docs/.vuepress/InputExample.vue
+++ docs/.vuepress/InputExample.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts" generic="T extends Record<string, unknown>">
 import {cloneDeep} from 'lodash-es'
-import {InputComplex, Scheme, Viewport} from 'tweeq'
+import {InputButton, InputComplex, Scheme, Viewport} from 'tweeq'
 import {ref, shallowRef} from 'vue'
 
 interface Props {
@@ -18,17 +18,30 @@ const modelValue = ref(props.initialValue)
 function update(value: any) {
 	modelValue.value = value
 }
+
+const isFullScreen = ref(false)
+
+function fullScreen() {
+	isFullScreen.value = !isFullScreen.value
+}
 </script>
 
 <template>
 	<ClientOnly>
-		<Viewport class="Example">
+		<Viewport class="Example" :class="{'full-screen': isFullScreen}">
+			<InputButton
+				class="full-screen-button"
+				:icon="isFullScreen ? 'mdi:fullscreen-exit' : 'mdi:fullscreen'"
+				:label="isFullScreen ? 'Exit Full Screen' : 'Full Screen'"
+				subtle
+				@click="fullScreen"
+			/>
 			<div class="input">
 				<slot :modelValue="modelValue" :update="update" :options="options" />
 			</div>
 
 			<div class="options">
-				<InputComplex v-model="options" :scheme="scheme as any" />
+				<InputComplex v-model="options" :scheme="scheme" />
 			</div>
 		</Viewport>
 	</ClientOnly>
@@ -37,11 +50,33 @@ function update(value: any) {
 <style lang="stylus" scoped>
 .Example
 	position relative
-	padding 2rem 0
+	padding 4rem 0 2rem
 	display grid
 	grid-template-columns min-content 1fr
 	gap 4rem
+	background-color var(--tq-color-background)
+
+	&.full-screen
+		position fixed
+		display flex
+		flex-direction column
+		justify-content center
+		align-items center
+		z-index 1000
+		inset 0
+
+		.options
+			display none
 
 .input
 	width 15rem
+
+.full-screen-button
+	position absolute
+	top 1rem
+	right 0rem
+
+	.full-screen &
+		top 2rem
+		right 2rem
 </style>
