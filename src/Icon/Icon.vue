314b22b6a4bcf60b7ea7571d36f3e58a4ae097b3
Baku Hashimoto <mail@baku89.com>
2025-03-21 09:16:40 +0900

    Add Icon

---
+++ src/Icon/Icon.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {Icon as Iconify} from '@iconify/vue'
+import {computed} from 'vue'
+
+const props = defineProps<{
+	icon: string
+}>()
+
+const icon = computed(() => {
+	if (props.icon.startsWith('char:')) {
+		return {
+			type: 'char',
+			value: props.icon.slice(5),
+		}
+	}
+
+	if (props.icon.startsWith('fill:')) {
+		return {
+			type: 'fill',
+			value: props.icon.slice(5),
+		}
+	}
+
+	return {
+		type: 'iconify',
+		value: props.icon,
+	}
+})
+</script>
+
+<template>
+	<Iconify v-if="icon.type === 'iconify'" :icon="icon.value" />
+	<div v-else-if="icon.type === 'char'" class="TqIcon char">
+		{{ icon.value }}
+	</div>
+	<svg
+		v-else-if="icon.type === 'fill'"
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		class="TqIcon fill"
+	>
+		<path fill="currentColor" :d="icon.value" />
+	</svg>
+</template>
+
+<style lang="stylus" scoped>
+@import '../common.styl'
+
+
+.TqIcon.char
+	line-height 100%
+	text-align center
+	display flex
+	align-items center
+	justify-content center
+</style>
