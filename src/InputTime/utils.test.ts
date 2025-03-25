ccb711ca4819f36c6cdd67d9e24587ad1f90a2d8
Baku Hashimoto <mail@baku89.com>
2025-03-25 15:51:22 +0900

    Add tests for parseTimecode

---
+++ src/InputTime/utils.test.ts
@@ -0,0 +1,44 @@
+import {describe, expect, it} from 'vitest'
+
+import {parseTimecode} from './utils'
+
+describe('parseTimecode', () => {
+	it('should parse timecode split by colon', () => {
+		expect(parseTimecode('00:00:00', 24)).toBe(0)
+		expect(parseTimecode('00:00:00', 30)).toBe(0)
+		expect(parseTimecode('1:00', 24)).toBe(24)
+		expect(parseTimecode('1:00', 30)).toBe(30)
+		expect(parseTimecode('1:00:00', 60)).toBe(60 * 60)
+		expect(parseTimecode('120:00', 60)).toBe(60 * 120)
+	})
+
+	it('should parse digits with unit', () => {
+		expect(parseTimecode('100f', 24)).toBe(100)
+		expect(parseTimecode('100F', 24)).toBe(100)
+		expect(parseTimecode('100Frames', 24)).toBe(100)
+
+		expect(parseTimecode('5s', 30)).toBe(150)
+		expect(parseTimecode('5sec', 30)).toBe(150)
+		expect(parseTimecode('5secs', 30)).toBe(150)
+		expect(parseTimecode('5second', 30)).toBe(150)
+		expect(parseTimecode('5seconds', 30)).toBe(150)
+
+		expect(parseTimecode('10m', 30)).toBe(18000)
+		expect(parseTimecode('10min', 30)).toBe(18000)
+		expect(parseTimecode('10mins', 30)).toBe(18000)
+		expect(parseTimecode('10minutes', 30)).toBe(18000)
+
+		expect(parseTimecode('10h', 30)).toBe(1080000)
+		expect(parseTimecode('10hr', 30)).toBe(1080000)
+		expect(parseTimecode('10hrs', 30)).toBe(1080000)
+		expect(parseTimecode('10hour', 30)).toBe(1080000)
+		expect(parseTimecode('10hours', 30)).toBe(1080000)
+	})
+
+	it('should parse digits with unit separated by whitespaces', () => {
+		expect(parseTimecode('10  f', 30)).toBe(10)
+		expect(parseTimecode('10  s', 30)).toBe(300)
+		expect(parseTimecode('10  m', 30)).toBe(30 * 60 * 10)
+		expect(parseTimecode('10  h', 30)).toBe(1080000)
+	})
+})
--- src/InputTime/utils.ts
+++ src/InputTime/utils.ts
@@ -35,26 +35,28 @@ export function parseTimecode(timecode: string, frameRate: number) {
 		let frames = 0
 
 		for (let i = 0; i < digits.length; i++) {
-			frames += digits[i] * Math.pow(60, i)
+			const multiplier =
+				i === 0 ? 1 : i === 1 ? frameRate : frameRate * Math.pow(60, i - 1)
+			frames += digits[i] * multiplier
 		}
 
 		return frames
 	}
 
 	// Matches with "s", "sec", "secs", "second", "seconds"
-	if (/s(ec(ond)?s?)?$/.test(timecode)) {
+	if (/^[0-9.]+\s*s(ec(ond)?s?)?$/.test(timecode)) {
 		const seconds = parseFloat(timecode)
 		return isNaN(seconds) ? null : Math.round(seconds * frameRate)
 	}
 
 	// Matches with "m", "min", "mins", "minute", "minutes"
-	if (/m(in(ute)?s?)?$/.test(timecode)) {
+	if (/^[0-9.]+\s*m(in(ute)?s?)?$/.test(timecode)) {
 		const minutes = parseFloat(timecode)
 		return isNaN(minutes) ? null : Math.round(minutes * frameRate * 60)
 	}
 
 	// Matches with "h", "hr", "hrs", "hour", "hours"
-	if (/h((ou)?r)?s?$/.test(timecode)) {
+	if (/^[0-9.]+\s*h((ou)?r)?s?$/.test(timecode)) {
 		const hours = parseFloat(timecode)
 		return isNaN(hours) ? null : Math.round(hours * frameRate * 3600)
 	}
